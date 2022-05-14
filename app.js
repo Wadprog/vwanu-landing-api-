fb9ee9794b2a7dff86b24bf3dec2cd977cc01b1b
Wadson <vavalson@hotmail.com>
2022-05-14 00:00:50 -0400

    added gervens frontend

--- app.js
+++ app.js
@@ -18,15 +18,18 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public', 'out')))
 
 // connecting to database
 connectDB()
 
-app.use('/', indexRouter)
+// app.use('/', indexRouter)
 app.use('/users', usersRouter)
 app.use('/tester', emailRouter)
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'out', 'index.html'))
+})
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404))
